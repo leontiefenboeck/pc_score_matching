7ddde03865f5d4dd8e83e0c2b00e2ae0aae3a8f3
Leon <tiefenboeck@student.tugraz.at>
2024-10-05 23:33:45 +0200

    inculded and descirbed plots in thesis

--- code/gmm/plot_data.py
+++ code/gmm/plot_data.py
@@ -2,21 +2,18 @@ import data
 import matplotlib.pyplot as plt
 
 datasets = ['halfmoons', 'spirals', 'board']
-fig, ax = plt.subplots(1, 3, figsize=(30, 6))
 
-for d, a in zip(datasets, ax):
-    x = data.get_2d(d, 10000, 42)
-    a.scatter(x[:, 0], x[:, 1], 
+for d in datasets:
+    x = data.get_2d(d, 10000 if d == 'board' else 6000, 42)
+    # plt.figure(figsize=(8, 5))
+    plt.scatter(x[:, 0], x[:, 1], 
                 c='dodgerblue',    # Set point color
                 s=20,              # Set point size
-                alpha=0.7,         # Add transparency to points
+                alpha=0.6,         # Add transparency to points
                 edgecolor='k',      # Black edge for contrast
                 linewidth=0.1)      # Thin edge
 
-    # Remove ticks and labels as per original
-    a.set_title(f'"{d}"', fontsize=30, fontweight='bold', pad=20)
-    a.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
-    # a.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.7)
-    a.axis('off')
-
-plt.savefig(f"datasets.png", format='png')
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(f"{d}.png", format='png', bbox_inches='tight')
+    plt.clf()
